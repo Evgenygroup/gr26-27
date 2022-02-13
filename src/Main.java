3568b351dc9ecd38768ef409270dc7ec919aab04
Evgenygroup <mailfrom61@gmail.com>
2022-02-13 15:41:10 +0100

    lesson 1 04.02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        // 1.write a program to print the sum of two numbers
+        //44`+20 ->64
+
+        System.out.println("44+20="+(44+20));
+        System.out.println("Hello "+"world");
+        System.out.println();
+
+
+        //2.write a program to divide  two numbers and print on the screen 10 2
+        System.out.println(10/2);
+        //3.write a program to print the result of the following operstions:
+        //5+8*6
+        System.out.println(5+8*6);
+        //(5+9)%9
+        System.out.println((5+9)%9);
+
+        //4. write a program that displays the square of 4
+        System.out.println(4*4);
+        //5. item a price is 1000 Euro, item b price is 500 Euro. If a customer buys two items
+        //he/she receives a discount of 100 Euro. Display the result if a customer
+        //purchases both items and gets a discount
+        System.out.println(1000+500-100);
     }
 }
